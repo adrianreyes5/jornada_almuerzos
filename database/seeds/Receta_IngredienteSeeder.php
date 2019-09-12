1454962f042158e36403ab6c025784f3c0921f2c
Adrian Reyes <adriandmc05@gmail.com>
2019-09-12 04:17:05 -0500

    Modificando Seeder

--- database/seeds/Receta_IngredienteSeeder.php
+++ database/seeds/Receta_IngredienteSeeder.php
@@ -15,140 +15,240 @@ class RecetaredienteSeeder extends Seeder
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  1,
             'ingrediente_id'    =>  3,
-            'cantidad'      =>  1
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  1,
-            'ingrediente_id'    =>  8,
-            'cantidad'      =>  2
+            'ingrediente_id'    =>  6,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  1,
+            'ingrediente_id'    =>  1,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  1,
             'ingrediente_id'    =>  9,
-            'cantidad'      =>  1
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  1,
-            'ingrediente_id'    =>  4,
-            'cantidad'      =>  2
-        ]);
-        DB::table('ingrediente_recetas')->insert([
-            'receta_id'         =>  1,
-            'ingrediente_id'    =>  8,
-            'cantidad'      =>  6
+            'ingrediente_id'    =>  2,
+            'cantidad'      =>  rand(1,6)
         ]);
 
         /** Receta 2 */
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  2,
-            'ingrediente_id'    =>  2,
-            'cantidad'      =>  2
+            'ingrediente_id'    =>  4,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  2,
-            'ingrediente_id'    =>  6,
-            'cantidad'      =>  3
+            'ingrediente_id'    =>  3,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  2,
-            'ingrediente_id'    =>  7,
-            'cantidad'      =>  1
+            'ingrediente_id'    =>  9,
+            'cantidad'      =>  rand(1,6)
         ]);
 
         /** Receta 3 */
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  3,
             'ingrediente_id'    =>  10,
-            'cantidad'      =>  1
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  3,
-            'ingrediente_id'    =>  4,
-            'cantidad'      =>  2
+            'ingrediente_id'    =>  3,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  3,
-            'ingrediente_id'    =>  5,
-            'cantidad'      =>  1
+            'ingrediente_id'    =>  8,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  3,
             'ingrediente_id'    =>  2,
-            'cantidad'      =>  4
+            'cantidad'      =>  rand(1,6)
         ]);
 
         /** Receta 4 */
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  4,
-            'ingrediente_id'    =>  7,
-            'cantidad'      =>  3
-        ]);
-        DB::table('ingrediente_recetas')->insert([
-            'receta_id'         =>  4,
-            'ingrediente_id'    =>  2,
-            'cantidad'      =>  1
+            'ingrediente_id'    =>  1,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  4,
             'ingrediente_id'    =>  6,
-            'cantidad'      =>  2
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  4,
+            'ingrediente_id'    =>  8,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  4,
+            'ingrediente_id'    =>  4,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  4,
             'ingrediente_id'    =>  9,
-            'cantidad'      =>  7
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  4,
-            'ingrediente_id'    =>  3,
-            'cantidad'      =>  1
-        ]);
-        DB::table('ingrediente_recetas')->insert([
-            'receta_id'         =>  4,
-            'ingrediente_id'    =>  5,
-            'cantidad'      =>  4
+            'ingrediente_id'    =>  10,
+            'cantidad'      =>  rand(1,6)
         ]);
 
         /** Receta 5 */
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  5,
-            'ingrediente_id'    =>  4,
-            'cantidad'      =>  3
+            'ingrediente_id'    =>  2,
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  5,
             'ingrediente_id'    =>  9,
-            'cantidad'      =>  2
+            'cantidad'      =>  rand(1,6)
         ]);
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  5,
-            'ingrediente_id'    =>  8,
-            'cantidad'      =>  2
+            'ingrediente_id'    =>  6,
+            'cantidad'      =>  rand(1,6)
         ]);
 
         /** Receta 6 */
         DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  6,
-            'ingrediente_id'    =>  1,
-            'cantidad'      =>  3
-        ]);
-        DB::table('ingrediente_recetas')->insert([
-            'receta_id'         =>  6,
-            'ingrediente_id'    =>  7,
-            'cantidad'      =>  2
-        ]);
-        DB::table('ingrediente_recetas')->insert([
-            'receta_id'         =>  6,
-            'ingrediente_id'    =>  10,
-            'cantidad'      =>  2
-        ]);DB::table('ingrediente_recetas')->insert([
-            'receta_id'         =>  6,
             'ingrediente_id'    =>  3,
-            'cantidad'      =>  3
-        ]);DB::table('ingrediente_recetas')->insert([
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
             'receta_id'         =>  6,
+            'ingrediente_id'    =>  4,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  6,
+            'ingrediente_id'    =>  8,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  6,
+            'ingrediente_id'    =>  1,
+            'cantidad'      =>  rand(1,6)
+        ]);
+
+        /** Receta 7 */
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  7,
+            'ingrediente_id'    =>  2,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  7,
+            'ingrediente_id'    =>  6,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  7,
+            'ingrediente_id'    =>  8,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  7,
+            'ingrediente_id'    =>  2,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  7,
             'ingrediente_id'    =>  9,
-            'cantidad'      =>  5
+            'cantidad'      =>  rand(1,6)
+        ]);
+
+        /** Receta 8 */
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  8,
+            'ingrediente_id'    =>  1,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  8,
+            'ingrediente_id'    =>  5,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  8,
+            'ingrediente_id'    =>  2,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  8,
+            'ingrediente_id'    =>  9,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  8,
+            'ingrediente_id'    =>  10,
+            'cantidad'      =>  rand(1,6)
+        ]);
+
+        /** Receta 9 */
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  9,
+            'ingrediente_id'    =>  3,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  9,
+            'ingrediente_id'    =>  7,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  9,
+            'ingrediente_id'    =>  5,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  9,
+            'ingrediente_id'    =>  9,
+            'cantidad'      =>  rand(1,6)
+        ]);
+
+        /** Receta 10 */
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  10,
+            'ingrediente_id'    =>  2,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  10,
+            'ingrediente_id'    =>  6,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  10,
+            'ingrediente_id'    =>  5,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  10,
+            'ingrediente_id'    =>  9,
+            'cantidad'      =>  rand(1,6)
+        ]);
+        DB::table('ingrediente_recetas')->insert([
+            'receta_id'         =>  10,
+            'ingrediente_id'    =>  10,
+            'cantidad'      =>  rand(1,6)
         ]);
     }
 }
